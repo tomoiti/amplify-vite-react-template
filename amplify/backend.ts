761f1d8dc93eb3b1f63ddb3f50565236fbc19f3d
Ricardo Ushisima <ricardo.ushisima@zbra.dev>
2024-08-13 08:39:17 -0300

    add storage backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,11 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { sayHello } from './functions/say-hello/resource';
+import { storage } from './storage/resource';
 
 defineBackend({
   auth,
   data,
   sayHello,
+  storage,
 });
